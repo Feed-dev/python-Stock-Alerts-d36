9b5d7eca83cda330413f5476c2485f479a53a36f
federico Lubbert <federico.lubbert@gmail.com>
2024-03-12 22:11:07 +0100

    init

--- main.py
+++ main.py
@@ -1,47 +1,56 @@
+import requests
+from newsapi import NewsApiClient
+from twilio.rest import Client
+
+# fill in your own API keys and phone numbers
+newsapi = NewsApiClient(api_key="YOUR OWN API KEY FROM NEWSAPI")
+account_sid = "YOUR TWILIO ACCOUNT SID"
+auth_token = "YOUR TWILIO AUTH TOKEN"
+client = Client(account_sid, auth_token)
+
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-
-STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+STOCK_ENDPOINT = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=APIKEY'
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+# request stock data
+r = requests.get(STOCK_ENDPOINT)
+data = r.json()
+time_series = data['Time Series (Daily)']
 
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+# get prices and calculate difference
+yesterday = list(time_series.keys())[0]
+yesterday_closing_price = time_series[yesterday]['4. close']
+print(yesterday_closing_price)
 
-#TODO 2. - Get the day before yesterday's closing stock price
+day_before_yesterday = list(time_series.keys())[1]
+day_before_yesterday_closing_price = time_series[day_before_yesterday]['4. close']
+print(day_before_yesterday_closing_price)
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+positive_difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+up_down = None
+if positive_difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+percentage_difference = (positive_difference / float(yesterday_closing_price)) * 100
+print(percentage_difference)
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+# use the News API to get articles when the difference is greater than 5%
+if percentage_difference > 0.1:
+    all_articles = newsapi.get_everything(q=COMPANY_NAME, language='en', sort_by='relevancy')
+    first_three_articles = all_articles['articles'][:3]
 
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+    articles = all_articles['articles']
+    formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in articles]
+    print(formatted_articles)
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
-
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+    # send the articles via SMS
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_='VIRTUAL_TWILIO_NUMBER',
+            to='VERIFIED_NUMBER'
+        )
+        print(message.status)
